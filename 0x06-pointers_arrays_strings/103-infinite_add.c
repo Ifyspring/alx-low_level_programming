518e6009ec856bb5aa5f2600c8c33520a66c1cdd
Ifyspring <ozurumbaimaureen@yahoo.com>
2022-06-17 15:36:36 -0700

    infinte

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -20,25 +20,26 @@ while (n2[j] != '\0')
 	j++;
 if (i > j)
 	l = i;
-else
+	else
 	l = j;
 if (l + 1 > size_r)
 	return (0);
-	r[l] = '\0';
+r[l] = '\0';
 for (k = l - 1 ; k >= 0 ; k--)
 	{
 	i--;
 	j--;
 if (i >= 0)
 	f = n1[i] - '0';
-else
+	else
 	f = 0;
-	if (j >= 0)
+if (j >= 0)
 	s = n2[j] - '0';
 	else
 	s = 0;
 	r[k] = (f + s + d) % 10 + '0';
 	d = (f + s + d) / 10;
+	}
 	if (d == 1)
 	{
 	r[l + 1] = '\0';
@@ -46,7 +47,7 @@ if (l + 2 > size_r)
 	return (0);
 while (l-- >= 0)
 	r[l + 1] = r[l];
-	r[0] = d + '0';
+r[0] = d + '0';
 	}
 	return (r);
 }
