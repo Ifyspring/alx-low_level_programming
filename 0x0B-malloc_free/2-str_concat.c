db45822fd02b4354b001157faf5d6bb92fbfd76a
Ifyspring <ozurumbaimaureen@yahoo.com>
2022-06-28 12:25:29 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,40 @@
-#include <string.h>
+#include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - concatenate two string
- * @s1: string 1
- * @s2: string 2
- * Description: return the required result
- * Return: retunr char pointer
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new space in memory or null
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	char *ptr = NULL;
+	char *strDup;
+	int a, b;
 
-	while (s1[i] != '\0')
-	i++;
-	while (s2[j] != '\0')
-	j++;
-	ptr = (char *)malloc((i + j + 1)  * sizeof(char));
-	if (ptr == NULL)
-	return (NULL);
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
-	i = 0;
-	j = 0;
-	for (i = 0; s1[i] != '\0'; i++)
+	a = b = 0;
+	while (s1[a] != '\0')
+	a++;
+	while (s2[b] != '\0')
+	b++;
+	strDup = malloc(sizeof(char) * (a + b + 1));
+	if (strDup == NULL)
+	return (NULL);
+	a = b = 0;
+	while (s1[a] != '\0')
 	{
-	ptr[i] = s1[i];
+	strDup[a] = s1[a];
+	a++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	while (s2[b] != '\0')
 	{
-	ptr[i++] = s2[j];
+	strDup[a] = s2[b];
+	a++, b++;
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	strDup[a] = '\0';
+	return (strDup);
 }
