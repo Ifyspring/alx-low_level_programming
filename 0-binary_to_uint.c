8b4c41679a1a900ca8f048b86a7fd7628b3dd020
Ifyspring <ozurumbaimaureen@yahoo.com>
2022-07-21 08:34:08 -0700

    binary to unit

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,33 +1,49 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
+ */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+	;
+	return (i);
+}
+
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int result, tmp, expo;
 
 	if (!b)
 	return (0);
-	ui = 0;
-	for (len = 0; b[len] != '\0'; len++)
-		;
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 	{
-	if (b[len] != '0' && b[len] != '1')
-	{
+	if (b[i] != '0' && b[i] != '1')
 	return (0);
+	tmp = _stoi(b[i]);
+	result += tmp * expo;
 	}
-	if (b[len] & 1)
-	{
-	ui += base_two;
-	}
-	}
-	return (ui);
+	return (result);
 }
