3a6aaa691e5956bbd5e8ab807b4dd5e00cb26504
Ifyspring <ozurumbaimaureen@yahoo.com>
2022-09-06 02:31:28 -0700

    create dynamic

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first argument
+ * @s2: second argument
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+	{
+	if (s1[i] != s2[i])
+	return (s1[i] - s2[i]);
+i++;
+	}
+	return (0);
+}
